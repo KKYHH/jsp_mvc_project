cb91d3ad8ca8e25dc73124ddb43ac826c5eafb49
KYHH <kyhh93@naver.com>
2023-04-26 14:48:31 +0900

    TEST

---
+++ src/README.md
@@ -0,0 +1,8 @@
+# MVC ㅍㅐ턴을 적용한 JSP 프로젝트
+
+- DB: MARIADB 10.11.2
+- Java: 17
+- Servilet: 4.0.1
+- Tomcat: 9.0.73
+- JSTL: 1.2
+``
---
+++ src/main/java/com/playdata/util/jdbcServletContextDbUtil.java
@@ -0,0 +1,12 @@
+package com.playdata.util;
+
+import javax.servlet.ServletContext;
+import java.sql.Connection;
+
+public class jdbcServletContextDbUtil implements DbUtil{
+
+    @Override
+    public Connection getConnection(ServletContext servletContext) {
+        return (Connection) servletContext.getAttribute("conn");
+    }
+}
